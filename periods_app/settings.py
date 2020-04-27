5421d3f55a1445c1dcf95f989a2d81222cdd5c55
Sandeep <rayanuthalas@gmail.com>
2020-04-28 00:08:26 +0530

    Redis

--- periods_app/settings.py
+++ periods_app/settings.py
@@ -96,7 +96,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [os.environ['REDIS_URL']],
         },
     },
 }

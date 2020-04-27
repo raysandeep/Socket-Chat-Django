8e65e36039ce30be72d6f81149233b170c681fac
Sandeep <rayanuthalas@gmail.com>
2020-04-28 00:00:30 +0530

    typo

--- app/managers.py
+++ app/managers.py
@@ -9,7 +9,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email ,username=username **extra_fields)
+        user = self.model(email=email ,username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
